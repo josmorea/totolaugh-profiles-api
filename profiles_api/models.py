e119a9648bb1da14231cbb097a2b772d9656086a
Mark Winterbottom <mark@londonappdeveloper.com>
2020-11-04 08:30:32 +0000

    Update models.py

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,23 +12,23 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address')
 
-            email = self.normalize_email(email)
-            user = self.model(email=email, name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
 
-            user.set_password(password)
-            user.save(using=self._db)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
+        
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-        def create_user(self, email, name, password):
-            """Create and save a new superuser with given details"""
-            user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using=self._db)
-
-            return user
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
